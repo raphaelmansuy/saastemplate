e7fcd47af65f5b367cf803673dceafdc1f606040
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2023-06-25 14:35:56 +0800

    add playwright

---
+++ e2e/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+ 
+test('should navigate to the about page', async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto('http://localhost:3000/')
+  //await expect(page.locator('text')).toContainText('A demo page')
+})
