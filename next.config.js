92739bae193e1d24eff32ffb20b700d40978663c
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2023-06-25 10:22:28 +0800

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  ignorePatterns: [
-    '**/*.stories.tsx',
-    '**/*.stories.ts',
-    '**/*.test.tsx',
-    '**/*.test.ts',
-  ],
+  webpack: (config, { isServer }) => {
+    // Exclude files or directories from being processed by Next.js
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.(stories|test)\.(tsx?|ts?)$/,
+        loader: 'ignore-loader',
+      });
+    }
+
+    return config;
+  },
 }
